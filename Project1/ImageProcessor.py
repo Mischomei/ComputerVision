ab421ff8af6ce452b6284602242eeeac233dd6ce
Mischomei <43608330+Mischomei@users.noreply.github.com>
2025-05-22 18:01:13 +0200

    Over 9000 Morphologische Operationen

--- Project1/ImageProcessor.py
+++ Project1/ImageProcessor.py
@@ -1,9 +1,11 @@
 import time
+from xmlrpc.client import MININT
 
 import cv2 as cv
 import numpy as np
 import imutils
 import os
+from numpy.polynomial import Polynomial as poly
 
 from cv2 import imshow
 
@@ -36,102 +38,193 @@ class ImageProcessor:
         print(f"Fehler: {mean_error/len(objPoints)}")
 
 
-    def find_edges(self, image): #Zwischenzeitliche Kantenerkennung
+    def find_edges(self, image, masks): #Zwischenzeitliche Kantenerkennung
         img = image.copy()
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         gray_blur = cv.GaussianBlur(gray, (5, 5), 0)
         #ret, thresh = cv.threshold(gray_blur, 100, 255, cv.THRESH_BINARY)
         edges = cv.Canny(gray_blur, 25, 150)
+        if masks:
+            for mask in masks:
+                copycopy = img.copy()
 
-        cv.imshow("image", edges)
-        cv.waitKey(0)
-        cv.destroyWindow("image")
+                comb = cv.bitwise_and(edges, edges, mask=mask)
+                self.showimg(self.lines(comb, img), "edges")
         return edges
 
-
-
-    def find_contours(self, image): # Zwischenzeitliche Markererkennung
-        img = image
-
-        #Kantenerkennung // Edge Detection
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        gray_blur = cv.GaussianBlur(gray, (5, 5), 0)
-        edges = cv.Canny(gray_blur, 25, 125, apertureSize=3)
-        print(edges)
-
-        #Konturenerkennung // Contour Detection
-        contours = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
-
-
-        imgcopy = img.copy()
-        cv.drawContours(imgcopy, contours, -1, (0, 255, 0), 2)
-
-        cv.imshow("imagecopy", imgcopy)
-        cv.waitKey(0)
-        cv.destroyWindow("imagecopy")
-
-        return contours
-
-    def find_corners(self, img, edges):
-        img_copy = img.copy()
-
-        gray = cv.cvtColor(img_copy, cv.COLOR_BGR2GRAY)
-        #gray_blur = cv.GaussianBlur(gray, (5, 5), 0)
-
-        corners = cv.cornerHarris(gray, 2, 3, 0.04)
-
-        dst = cv.dilate(corners, None)
-        ret, dst = cv.threshold(dst, 0.01 * dst.max(), 255, 0)
-        dst = np.uint8(dst)
-
-        # find centroids
-        ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
-
-        # define the criteria to stop and refine the corners
-        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-        corners = cv.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
-
-        img_copy[dst > 0.01 * dst.max()] = [0, 0, 255]
-
-        res = np.hstack((centroids, corners))
-        res = np.uint8(res)
-        img_copy[res[:, 1], res[:, 0]] = [0, 0, 255]
-        img_copy[res[:, 3], res[:, 2]] = [0, 255, 0]
-
-
-        cv.imshow("image", img_copy)
-
-        cv.waitKey(0)
-        cv.destroyWindow("image")
-        return corners
-
-    def get_color_channels(self, img):
+    def get_color_channels(self, img, masks=None):
         img_copy = img.copy()
         b, g, r = cv.split(img_copy)
         blank = np.zeros(img.shape[:2], dtype="uint8")
 
+        #Gaussian Blur
+        b_blur = cv.GaussianBlur(b, (5, 5), 0)
+        g_blur = cv.GaussianBlur(g, (5, 5), 0)
+        r_blur = cv.GaussianBlur(r, (5, 5), 0)
+
+        #Thresholding
+        b_thresh = cv.threshold(b_blur, 95, 255, cv.THRESH_BINARY_INV)[1]
+        g_thresh = cv.threshold(g_blur, 95, 255, cv.THRESH_BINARY_INV)[1]
+        r_thresh = cv.threshold(r_blur, 95, 255, cv.THRESH_BINARY_INV)[1]
+        combthresh = cv.bitwise_or(b_thresh, g_thresh, r_thresh)
+        threshmask = cv.morphologyEx(masks[0], cv.MORPH_ERODE, (20, 20), iterations=2)
+        threshmask = cv.morphologyEx(threshmask, cv.MORPH_CLOSE, None, iterations=10)
+        threshmask = cv.morphologyEx(threshmask, cv.MORPH_OPEN, (20, 20), iterations=1)
+        self.showimg(threshmask, "threshmask")
+        combthresh = cv.bitwise_and(combthresh, combthresh, mask=threshmask)
+        edgesthresh = cv.Canny(combthresh, 25, 150, apertureSize=3)
+        self.showimg(edgesthresh, "edgesthresh")
+        self.showimg(self.lines(edgesthresh, img_copy), "edgesthreshlines")
         #Finde edges
-        b_edges = cv.Canny(cv.GaussianBlur(b, (3,3), 0), 25, 125, apertureSize=3)
-        g_edges = cv.Canny(cv.GaussianBlur(g, (3, 3), 0), 25, 125, apertureSize=3)
-        r_edges = cv.Canny(cv.GaussianBlur(r, (3, 3), 0), 25, 125, apertureSize=3)
-        cv.imshow("blue", b_edges)
-        cv.imshow("green", g_edges)
-        cv.imshow("red", r_edges)
+        b_edges = cv.Canny(b_blur, 25, 100, apertureSize=3)
+        g_edges = cv.Canny(g_blur, 25, 100, apertureSize=3)
+        r_edges = cv.Canny(r_blur, 25, 100, apertureSize=3)
+        combined = cv.bitwise_or(b_edges, g_edges, r_edges)
+        self.showimg(combined, "combined")
+        edgemask = masks[0]
+        edgemask = cv.morphologyEx(edgemask, cv.MORPH_OPEN, None, iterations=5)
+        combined = cv.bitwise_and(combined, combined, mask=edgemask)
+        edges = cv.morphologyEx(combined, cv.MORPH_DILATE, (3,3), iterations=2)
 
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-        cv.imshow("image", cv.bitwise_or(b_edges, g_edges, r_edges))
-        cv.waitKey(0)
-        cv.destroyWindow("image")
+        self.showimg(edges, "combinde")
+        self.showimg(self.lines(edges, img_copy), "Lines")
+        cons = cv.findContours(combined, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        self.lines(cons, img_copy)
+        return img_copy, b, g, r
 
-        return b, g, r
+    def lines(self, edge, img):
+
+        #Approxpoly
+        cons = cv.findContours(edge, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cons = imutils.grab_contours(cons)
+        c = max(cons, key=cv.contourArea)
+        #poly = cv.approxPolyDP(c, 0.02 * cv.arcLength(c, True), True)
+        poly = cv.approxPolyN(c, 6, approxCurve=np.ndarray([]),ensure_convex=True)
+        print(poly)
+        cv.drawContours(img, [poly], 0, (0, 0, 255), 5)
+        self.showimg(img, "lines")
+
+        for index, point in enumerate(poly[0].astype(int)):
+            cv.circle(img,(point[0], point[1]), 5, (0, 0, 255), -1)
+            cv.putText(img, str(index), (point[0], point[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        self.showimg(img, "lines")
+        #Hough Line Transform // Hough-Line-Transformation
+        imgcopy = img.copy()
+        edges = edge.copy()
+        rho = 1  # Abstandauflösung in Pixel
+        theta = np.pi / 180  # Winkelauflösung in Radiant
+        threshold = 20  # Mindestanzahl an Stimmen (Schnittpunkte in der Hough-Ebene)
+        min_line_length = 30  # Minimale Linienlänge. Linien kürzer als diese werden verworfen.
+        max_line_gap = 10 # Maximaler Abstand zwischen Liniensegmenten, um als eine Linie betrachtet zu werden.
+
+        lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                                minLineLength=min_line_length, maxLineGap=max_line_gap)
+
+        # Linien auf ein leeres Bild oder das Originalbild zeichnen (optional)
+        line_image = np.copy(imgcopy) * 0  # Erzeuge ein schwarzes Bild
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv.line(imgcopy, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+        #Corners from Lines
+        px1, py1, px2, py2, px3, py3, px4, py4, px5, py5, px6, py6, px7, py7, px8, py8 = line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1]
+
+
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            if px1 > x1:
+                px1 = x1
+                py1 = y1
+                px8 = x2
+                py8 = y2
+            if px2 < x2:
+                px2 = x2
+                py2 = y2
+                px7 = x1
+                py7 = y1
+            if py3 > y1:
+                py3 = y1
+                px3 = x1
+                px6 = x2
+                py6 = y2
+            if py4 < y2:
+                py4 = y2
+                px4 = x2
+                px5 = x1
+                py5 = y1
+
+
+        cv.circle(imgcopy, (px1, py1), 5, (0, 0, 0), -1)
+        #cv.circle(imgcopy, (px2, py2), 5, (0, 0, 0), -1)
+        cv.circle(imgcopy, (px3, py3), 5, (0, 0, 0), -1)
+        #cv.circle(imgcopy, (px4, py4), 5, (0, 0, 0), -1)
+        #cv.circle(imgcopy, (px5, py5), 5, (0, 0, 255), -1)
+        cv.circle(imgcopy, (px6, py6), 5, (0, 255, 0), -1)
+        #cv.circle(imgcopy, (px7, py7), 5, (255, 255, 255), -1)
+        cv.circle(imgcopy, (px8, py8), 5, (0, 255, 255), -1)
+
+
+        return imgcopy
+
+    #Drehen vom Bild
+    def rotate(self, img, angle=90):
+        img = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
+        return img
+
+    #Croppen vom Bild
+    def crop(self, img, cropx_s=None, cropx_e=None, cropy_s=None, cropy_e=None):
+        cxs = cropx_s if cropx_s else 0
+        cxe = cropx_e if cropx_e else img.shape[1]
+        cys = cropy_s if cropy_s else 0
+        cye = cropy_e if cropy_e else img.shape[0]
+        img_copy = img.copy()
+        return img_copy[cxs:cxe, cys:cye]
+
+    def color_mask(self, img, lower, upper, preprocess=False):
+        img_copy = img.copy()
+        img_copy = cv.cvtColor(img_copy, cv.COLOR_BGR2HSV)
+        mask = cv.inRange(img_copy, lower, upper)
+        mask = cv.threshold(mask, 2, 255, cv.THRESH_BINARY)[1]
+
+        self.showimg(mask, "mask")
+
+        return mask
+
+    def cornersto3d(self, corners):
+        if len(corners) == 4:
+            pass
+
+        #AUs 6 erkennbaren des Quaders ein Sechseck und daraus seine 3D koordinaten
+        if len(corners) == 6:
+            # Aus Ecken Kanten machen
+            pass
+
+
+    def refine_edges(self):
+        pass
 
     def calc_coords_from_edges(self):
         pass
 
     def calc_coords_from_corners(self):
         pass
+
+
+    def linesintersect(self, line1, line2):#
+        intersections = list()
+        x1, y1, x2, y2 = line1
+        x3, y3, x4, y4 = line2
+        line1_params = poly.fit((x1, x2), (y1, y2), 1)
+        line2_params = poly.fit((x3, x4), (y3, y4), 1)
+        print(line1_params)
+        print(line2_params)
+
+    #Anzeigen des Bildes
+    def showimg(self, img, name):
+        cv.imshow(name, img)
+        cv.waitKey(0)
+        cv.destroyWindow(name)
 
 
 
