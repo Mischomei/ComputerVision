af32cdce8be698379db52f6350eacd75e95cf212
Mischomei <43608330+Mischomei@users.noreply.github.com>
2025-05-22 18:04:35 +0200

    Was nicht funktioniert braucht man nicht

--- Project1/ImageProcessor.py
+++ Project1/ImageProcessor.py
@@ -38,20 +38,6 @@ class ImageProcessor:
         print(f"Fehler: {mean_error/len(objPoints)}")
 
 
-    def find_edges(self, image, masks): #Zwischenzeitliche Kantenerkennung
-        img = image.copy()
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        gray_blur = cv.GaussianBlur(gray, (5, 5), 0)
-        #ret, thresh = cv.threshold(gray_blur, 100, 255, cv.THRESH_BINARY)
-        edges = cv.Canny(gray_blur, 25, 150)
-        if masks:
-            for mask in masks:
-                copycopy = img.copy()
-
-                comb = cv.bitwise_and(edges, edges, mask=mask)
-                self.showimg(self.lines(comb, img), "edges")
-        return edges
-
     def get_color_channels(self, img, masks=None):
         img_copy = img.copy()
         b, g, r = cv.split(img_copy)
@@ -70,10 +56,8 @@ class ImageProcessor:
         threshmask = cv.morphologyEx(masks[0], cv.MORPH_ERODE, (20, 20), iterations=2)
         threshmask = cv.morphologyEx(threshmask, cv.MORPH_CLOSE, None, iterations=10)
         threshmask = cv.morphologyEx(threshmask, cv.MORPH_OPEN, (20, 20), iterations=1)
-        self.showimg(threshmask, "threshmask")
         combthresh = cv.bitwise_and(combthresh, combthresh, mask=threshmask)
         edgesthresh = cv.Canny(combthresh, 25, 150, apertureSize=3)
-        self.showimg(edgesthresh, "edgesthresh")
         self.showimg(self.lines(edgesthresh, img_copy), "edgesthreshlines")
 
         return img_copy, b, g, r
@@ -88,70 +72,10 @@ class ImageProcessor:
         poly = cv.approxPolyN(c, 6, approxCurve=np.ndarray([]),ensure_convex=True)
         print(poly)
         cv.drawContours(img, [poly], 0, (0, 0, 255), 5)
-        self.showimg(img, "lines")
-
         for index, point in enumerate(poly[0].astype(int)):
             cv.circle(img,(point[0], point[1]), 5, (0, 0, 255), -1)
             cv.putText(img, str(index), (point[0], point[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        self.showimg(img, "lines")
-        #Hough Line Transform // Hough-Line-Transformation
-        imgcopy = img.copy()
-        edges = edge.copy()
-        rho = 1  # Abstandauflösung in Pixel
-        theta = np.pi / 180  # Winkelauflösung in Radiant
-        threshold = 20  # Mindestanzahl an Stimmen (Schnittpunkte in der Hough-Ebene)
-        min_line_length = 30  # Minimale Linienlänge. Linien kürzer als diese werden verworfen.
-        max_line_gap = 10 # Maximaler Abstand zwischen Liniensegmenten, um als eine Linie betrachtet zu werden.
-
-        lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                                minLineLength=min_line_length, maxLineGap=max_line_gap)
-
-        # Linien auf ein leeres Bild oder das Originalbild zeichnen (optional)
-        line_image = np.copy(imgcopy) * 0  # Erzeuge ein schwarzes Bild
-        if lines is not None:
-            for line in lines:
-                x1, y1, x2, y2 = line[0]
-                cv.line(imgcopy, (x1, y1), (x2, y2), (255, 0, 0), 3)
-
-        #Corners from Lines
-        px1, py1, px2, py2, px3, py3, px4, py4, px5, py5, px6, py6, px7, py7, px8, py8 = line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1], line[0][0], line[0][1]
-
-
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            if px1 > x1:
-                px1 = x1
-                py1 = y1
-                px8 = x2
-                py8 = y2
-            if px2 < x2:
-                px2 = x2
-                py2 = y2
-                px7 = x1
-                py7 = y1
-            if py3 > y1:
-                py3 = y1
-                px3 = x1
-                px6 = x2
-                py6 = y2
-            if py4 < y2:
-                py4 = y2
-                px4 = x2
-                px5 = x1
-                py5 = y1
-
-
-        cv.circle(imgcopy, (px1, py1), 5, (0, 0, 0), -1)
-        #cv.circle(imgcopy, (px2, py2), 5, (0, 0, 0), -1)
-        cv.circle(imgcopy, (px3, py3), 5, (0, 0, 0), -1)
-        #cv.circle(imgcopy, (px4, py4), 5, (0, 0, 0), -1)
-        #cv.circle(imgcopy, (px5, py5), 5, (0, 0, 255), -1)
-        cv.circle(imgcopy, (px6, py6), 5, (0, 255, 0), -1)
-        #cv.circle(imgcopy, (px7, py7), 5, (255, 255, 255), -1)
-        cv.circle(imgcopy, (px8, py8), 5, (0, 255, 255), -1)
-
-
-        return imgcopy
+        return img
 
     #Drehen vom Bild
     def rotate(self, img, angle=90):
@@ -172,9 +96,6 @@ class ImageProcessor:
         img_copy = cv.cvtColor(img_copy, cv.COLOR_BGR2HSV)
         mask = cv.inRange(img_copy, lower, upper)
         mask = cv.threshold(mask, 2, 255, cv.THRESH_BINARY)[1]
-
-        self.showimg(mask, "mask")
-
         return mask
 
     def cornersto3d(self, corners):
