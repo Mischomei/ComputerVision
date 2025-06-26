0d3a90d6dd10bce68241e77f03edb84a36a726bc
Mischomei <43608330+Mischomei@users.noreply.github.com>
2025-06-26 21:44:45 +0200

    Inzwischen habe ich keinen Durchblick mehr hilfe pt.3

--- src/ImageProcessing/StereoProcessor.py
+++ src/ImageProcessing/StereoProcessor.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import numpy as np
+from numpy.f2py.auxfuncs import throw_error
+
+from src.ImageProcessing.ImageProcessor import ImageProcessor
 
 
-class StereoProcessor:
+class StereoProcessor(ImageProcessor):
     debug = False
     def __init__(self, debug=False):
+        super().__init__(debug)
         self.debug = debug
 
     def undistort_rectify(self, frameL, frameR, stereocam):
@@ -30,3 +34,31 @@ class StereoProcessor:
         zD = (stereocam.baseline*f_pixel) / disp[0]
         print(f"Distanz Kamera-Punkt: {abs(zD)}")
         return abs(zD)
+
+    def cons_stereo(self, imgleft, imgright, masks):
+        imgcopyr = imgright.copy()
+        imgcopyl = imgleft.copy()
+        leftmasks = list()
+        rightmasks = list()
+        for mask in masks:
+            leftmasks.append((mask[0], mask[2]))
+            rightmasks.append((mask[1], mask[2]))
+
+        rightimg = self.cons_per_color(imgcopyr, rightmasks)
+        if self.debug: self.showimg(rightimg, "cons_stereo_right")
+        leftimg = self.cons_per_color(imgcopyl, leftmasks)
+        if self.debug: self.showimg(leftimg, "cons_stereo_left")
+        return rightimg, leftimg
+
+
+    def combine_masks(self, leftmasks, rightmasks):
+        outmasks = list()
+
+        if leftmasks.keys() == rightmasks.keys():
+            for key in leftmasks.keys():
+                outmasks.append((leftmasks[key], rightmasks[key], key))
+        else:
+            throw_error("Rechte und Linke Maskenlisten haben nicht die gleichen Farben")
+
+        return outmasks
+
