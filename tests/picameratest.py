2914f5057a15671ac419ced33460ee06cb57a5e9
Mischomei <mikhail.itin@gmail.com>
2025-06-26 18:47:07 +0100

    Picamera kleine Änderungen

--- tests/picameratest.py
+++ tests/picameratest.py
@@ -1,18 +1,33 @@
 import threading
-
+import sys
+import time
+from pathlib import Path
+sys.path.insert(1, (Path.cwd().parent).as_posix())
 from src.PiCamera.Picamera import Picamera
+from src.ImageProcessing.ImageProcessor import ImageProcessor
+import cv2 as cv
+proc = ImageProcessor()
+cam1 = Picamera(0)
 
-cam1 = Picamera(1)
-cam2 = Picamera(2)
+cam2 = Picamera(1)
 
+cam1.preview()
+cam1.start()
+cam2.preview()
+cam2.start()
 
-stream1 = threading.Thread(cam1.preview())
-stream2 = threading.Thread(cam2.preview())
-capture1 = threading.Thread()
-stream1.start()
-stream2.start()
+while True:
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+    elif cv.waitKey(1) & 0xFF == ord("s"):
+        img = cam1.capture_normal()
+        proc.showimg(img, "img")
+#stream1 = threading.Thread(cam1.preview())
+#stream2 = threading.Thread(cam2.preview())
+#capture1 = threading.Thread()
+#stream1.start()
+#stream2.start()
 
-threading.Timer(60.0, stream1.join)
-threading.Timer(60.0, stream2.join)
+#threading.Timer(60.0, stream1.join)
 
-exit()
+#threading.Timer(60.0, stream2.join)
