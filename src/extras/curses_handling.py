88e83a547d43b0d77537f1da78ff834c4d4d72b2
Mischomei <43608330+Mischomei@users.noreply.github.com>
2025-06-26 22:00:06 +0200

    picamera tests

---
+++ src/extras/curses_handling.py
@@ -0,0 +1,16 @@
+import curses
+
+class Curses_Handler:
+    def __enter__(self):
+        self.stdsrc = curses.initscr()
+        curses.cbreak()
+        curses.noecho()
+        self.stdsrc.keypad(1)
+        SCREEN_HEIGHT, SCREEN_WIDTH = self.stdsrc.getmaxyx()
+        return self.stdsrc
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        curses.nocbreak()
+        self.stdsrc.keypad(0)
+        curses.echo()
+        curses.endwin()
